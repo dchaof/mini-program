7d85b22550f0451550c9e3c0e228f4a016a44f31
Duan Chaofan <dchaof888@163.com>
2022-07-02 16:36:45 +0800

    事件冒泡+路由跳转+生命周期钩子函数

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,16 +5,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    msg:'我爱你'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    console.log('onLoad')
 
+    // this.setData({
+    //   msg:'i love you'
+    // })
+    // console.log(this.data.msg)
+
+    //非自身钩子函数
+    // setTimeout(()=>{
+    //   this.setData({
+    //     msg : 'i love you'
+    //   })
+    //   console.log(this.data.msg)
+    // },2000)
   },
 
+  handleParent(){
+    console.log('父')
+  },
+  handleChild(){
+    console.log('子')
+  },
+
+  //进行路由跳转
+  toLogs(){
+    //写路径的时候一定要最前面写上 / 根目录  否则就会在该组件的路径上进行叠加
+    //navigateTo跳转后有返回按钮  
+    //redirectTo跳转后没有返回按钮 左上角有首页按钮
+    //reLaunch跳转后没有返回按钮 左上角有首页按钮
+    wx.navigateTo({
+      url: '/pages/logs/logs',
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
